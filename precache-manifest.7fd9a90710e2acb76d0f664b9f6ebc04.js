ed99b61da82502b93d57d58eec326c5578c4db90
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-11-08 10:33:15 +0200

    Updates

---
+++ precache-manifest.7fd9a90710e2acb76d0f664b9f6ebc04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8984a3e48439d8ff504a60e7eeee003a",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "9b8c273ca6e27221603c",
+    "url": "/react_tabs-with-router/static/css/main.bce74968.chunk.css"
+  },
+  {
+    "revision": "7aa4ae05e8613e1b483b",
+    "url": "/react_tabs-with-router/static/js/2.3b62ef88.chunk.js"
+  },
+  {
+    "revision": "9b8c273ca6e27221603c",
+    "url": "/react_tabs-with-router/static/js/main.991fcf14.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
